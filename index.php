fe4634e88be45374ffa1fae96ef99555abb7923e
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-07-28 05:21:16 +0200

    2016-07-28 php first example

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>php envato learning</title>
+    </head>
+    <body>
+        <h1>php evato learning</h1>
+        <?php
+            $name = $_GET['name'];
+            // echo "Hello, $name!";
+            // echo "<br />";
+            // echo 'Hello, $name!';
+            echo "<p>" . "Hello, " . $name . "!" . "</p>";
+        ?>
+        </body>
+</html>
